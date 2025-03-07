d9f2cc39e8410c2b4ccc3e4f43eb66ea2f7a1e75
RajithaKWG <rkrishan894@gmail.com>
2025-03-07 13:22:48 +0530

    setup clerk and convex

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,27 +1,15 @@
-import { Stack } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { ClerkProvider, ClerkLoaded, ClerkLoading } from "@clerk/clerk-expo";
-import { tokenCache } from '@/cache'
-
-
-const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
-
-if (!publishableKey) {
-  throw new Error(
-    "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
-  );
-}
+import InitialLayout from "../components/InitialLayout";
+import ClerkAndConvexProvider from "@/providers/ClerkAndConvexProvider";
 
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
-      <ClerkLoaded>
-        <SafeAreaProvider>
-          <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
-            <Stack screenOptions={{ headerShown: false }} />
-          </SafeAreaView>
-        </SafeAreaProvider>
-      </ClerkLoaded>
-    </ClerkProvider>
+    <ClerkAndConvexProvider>
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
+          <InitialLayout />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </ClerkAndConvexProvider>
   );
 }
---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+
+export default {
+    providers: [
+      {
+        domain: "https://unified-ram-71.clerk.accounts.dev/",
+        applicationID: "convex",
+      },
+    ]
+  };
